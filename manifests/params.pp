d71239645f4f33407d034da6da9b2bcc1ba8719c
Norbert Varzariu <loomsen@gmail.com>
2016-01-22 09:31:51 +0100

    restart cron service after config change

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,6 +7,7 @@ class rsnapshot::params {
   $config_backup_user            = 'root'
   $package_name                  = 'rsnapshot'
   $package_ensure                = 'present'
+  $cron_service_name             = 'crond'
   $cron_dir                      = '/etc/cron.d'
   $config_backup_levels          = [ 'daily', 'weekly', 'monthly' ]
   $config_backup_defaults        = true
@@ -32,7 +33,7 @@ class rsnapshot::params {
   $config_logpath                = '/var/log/rsnapshot'
   $config_logfile                = '/var/log/rsnapshot.log'  # unused, we are logging to $logpath/$host.log
   $config_lockpath               = '/var/run/rsnapshot'
-  $config_snapshot_root          = '/backup/'
+  $config_snapshot_root          = '/backup'
   $config_no_create_root         = undef # bool, true or false
   $config_verbose                = '2'
   $config_loglevel               = '4'
