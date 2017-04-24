a774e8b36099148c27f051b6278a3797c556af64
Norbert Varzariu <loomsen@gmail.com>
2017-04-24 20:43:49 +0200

    create /etc/tmpfiles.d dir on OS without systemd

--- manifests/install.pp
+++ manifests/install.pp
@@ -13,7 +13,11 @@ class rsnapshot::install {
   # ensure run directory exists (systemd clears /var/run, so rsnapshot can't create PID files after reboot - issue#12)
   $lockpath   = pick($rsnapshot::lockpath, $rsnapshot::params::config_lockpath, '/var/run/rsnapshot')
   $tmpfiles_d = '/etc/tmpfiles.d'
-  
+ 
+  file { "${tmpfiles_d}":
+    ensure => directory,
+  }
+
   file { "${tmpfiles_d}/rsnapshot.conf":
     ensure  => present,
     content => "D ${lockpath} 0755 root root -",
